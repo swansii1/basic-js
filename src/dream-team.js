dce01dd210d3902095545bad58afa569fe3a5923
Matvey Lebedev <matvey.swansii@gmail.com>
2024-12-24 02:17:43 +0300

    feat: completed task "count cats" and processing "dream team"

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,30 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let nameTeam = [];
+
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] === "string") {
+      nameTeam.push(members[i][0]);
+    } else {
+    }
+  }
+  return nameTeam.sort().join("").toUpperCase();
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
