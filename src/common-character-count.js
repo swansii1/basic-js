48b4e5bc692f7dabf93b5012c7738a80f610dcf7
Matvey Lebedev <matvey.swansii@gmail.com>
2024-12-25 19:36:12 +0300

    feat: completed task "getEmailDomen" and "characterCount"

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,11 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let arr1 = s1.split("");
+  let arr2 = s2.split("");
+
+  return arr1.filter((item) => {
+    const index = arr2.indexOf(item);
+    if (index !== -1) {
+      arr2.splice(index, 1);
+      return true;
+    }
+    return false;
+  }).length;
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an email address, return it's domain.
@@ -10,11 +10,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getEmailDomain(email) {
+  let index = 0;
+  for (let i = 0; i < email.length; i++) {
+    if (email[i] === "@") {
+      index = [i + 1];
+    }
+  }
+  return email.slice(index);
 }
 
 module.exports = {
-  getEmailDomain
+  getEmailDomain,
 };
